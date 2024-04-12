ac655ade51191d22e3e51b281d5e66370d7a2eef
phuoc09 <132633527+phuoc09@users.noreply.github.com>
2024-04-12 23:05:47 +0700

    Update read.py

--- 8-band/process/read.py
+++ 8-band/process/read.py
@@ -6,18 +6,12 @@ def decimal_to_signed_binary(decimal):
         binary = bin(decimal)[2:]
         # Điền các bit 0 phía trước nếu cần thiết để có độ dài 16 bit
         binary = binary.zfill(16)
-    else:
-        # Chuyển đổi số âm thành nhị phân bằng cách sử dụng 2's complement
-        # Tính giá trị tuyệt đối của số âm
-        abs_decimal = abs(decimal+1)
-        # Chuyển đổi giá trị tuyệt đối sang nhị phân
-        binary = bin(abs_decimal)[2:]
-        # Điền các bit 0 phía trước nếu cần thiết để có độ dài 15 bit
+    else :
+        # Chuyển đổi số dương thành nhị phân
+        binary = bin(-decimal)[2:]
+        # Điền các bit 0 phía trước nếu cần thiết để có độ dài 16 bit
         binary = binary.zfill(15)
-        # Thực hiện phép đảo bit
-        binary = ''.join('1' if bit == '0' else '0' for bit in binary)
-        # Thêm bit 1 đầu tiên để biểu diễn số âm
-        binary = '1' + binary
+        binary = '1'+ binary  
     return binary
 def Read(file_in,file_out):
     # Đọc tệp âm thanh WAV
@@ -27,5 +21,6 @@ def Read(file_in,file_out):
         for amplitude in audio_data:
             amplitude=decimal_to_signed_binary(amplitude)
             file.write(f"{amplitude}\n")
+
 
 
